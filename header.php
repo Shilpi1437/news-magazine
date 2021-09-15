741b7d99b9e0c9da3bab0228c17a3e4f976d85cb
Shilpi1437 <shilpinandani1437@gmail.com>
2021-09-15 20:07:22 +0530

    Update header.php

--- header.php
+++ header.php
@@ -61,7 +61,7 @@
                     <div class="collapse navbar-collapse" id="navbarCollapse">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="tech-index.html">Home</a>
+                                <a class="nav-link" href="index.php">Home</a>
                             </li>
                             <li
                                 class="nav-item dropdown has-submenu menu-large hidden-md-down hidden-sm-down hidden-xs-down">
